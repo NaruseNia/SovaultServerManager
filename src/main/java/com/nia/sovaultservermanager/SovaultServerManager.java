beb0b188980a1b602a3821f050b7475a3a887f03
NaruseNia <notshinsin0000@gmail.com>
2020-08-06 21:16:59 +0900

    fix somebugs

--- src/main/java/com/nia/sovaultservermanager/SovaultServerManager.java
+++ src/main/java/com/nia/sovaultservermanager/SovaultServerManager.java
@@ -11,12 +11,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
